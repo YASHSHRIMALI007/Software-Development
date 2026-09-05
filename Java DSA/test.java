f53227863a03eac5ca4fea3213fc421c4c06a4ef
YASH <shrimaliyash176@gmail.com>
2026-09-05 22:25:49 +0530

    Daily Software Development update - 2026-09-05 22:25

---
+++ Java DSA/test.java
@@ -0,0 +1,11 @@
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello, Java!");
+
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    System.out.println("Sum = " + sum);
+  }
+}
